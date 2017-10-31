b31782dc61a35e964e4050be674b8e4a4899ec07
Hack Reactor Student <student@hackreactor.com>
2017-10-30 21:01:30 -0700

    Worked through to POST, working on rendering new posts

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,8 +11,41 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+var http = require('http');
+var querystring = require('querystring');
+
+
+
+var data = {
+  results: [
+    {
+      username: 'David',
+      roomname: 'lobby',
+      text: 'Hello?'
+    },
+    {
+      username: 'Vlad',
+      roomname: 'lobby',
+      text: 'Anyone there?'
+    }
+  ]
+};
 
 var requestHandler = function(request, response) {
+  // var data = {
+  //   results: [
+  //     {
+  //       username: 'David',
+  //       roomname: 'lobby',
+  //       text: 'Hello?'
+  //     },
+  //     {
+  //       username: 'Vlad',
+  //       roomname: 'lobby',
+  //       text: 'Anyone there?'
+  //     }
+  //   ]
+  // };
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
@@ -27,14 +60,56 @@ var requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  // console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
   // The outgoing status.
+  var responseObj = 'Reponse Object Initialized';
   var statusCode = 200;
+  if ( request.url !== '/classes/messages') {
+    console.log('In the 404');
+    statusCode = 404;
+    response.writeHead(404, 'ERROR URL not found' );
+    response.end(responseObj);
+  } 
+
+  switch (request.method) {
+  case 'GET':
+    statusCode = 200;
+    responseObj = JSON.stringify(data);
+    break;
+  case 'OPTIONS':
+    statusCode = 200;
+    break;
+  case 'POST':
+    //console.log(response.on);
+    var body = '';
+    request.on('data', function(data) {
+      console.log('POST DATA??? ', data);
+      body += data;
+    });
+
+    request.on('end', function() {
+      data.results.push(querystring.parse(body));
+      responseObj = JSON.stringify(data);
+      console.log('response', responseObj);
+      response.end(responseObj);
+    });
+
+    statusCode = 201;
+    break;
+  case 'PUT':
+    statusCode = 201;
+    break;
+  default:
+    statusCode = 404;
+  }
+  
+  //TRIED TO ADD CONDITIONAL FOR DEALING WITH DIFFERENT ENDPOINT
 
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
-
+  // console.log('respons?: ', response.writeHead);
+  //console.log(headers['access-control-allow-methods']);
   // Tell the client we are sending them plain text.
   //
   // You will need to change this if you are sending something
@@ -43,8 +118,10 @@ var requestHandler = function(request, response) {
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  
+  
 
+  response.writeHead(statusCode, headers);
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
   // response.end() will be the body of the response - i.e. what shows
@@ -52,7 +129,8 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  // console.log(responseObj);
+  response.end(responseObj);
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -70,4 +148,30 @@ var defaultCorsHeaders = {
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10 // Seconds.
 };
+module.exports.requestHandler = requestHandler;
+
+
+
+
+    // var options = {
+    //   hostname: 'localhost',
+    //   port: 3000,
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   }
+    // };
+    // var req = http.request(options, function(res) {
+    //   console.log('STATUS:', res.statusCode);
+    //   console.log('HEADERS:', JSON.stringify(res.headers));
+    //   res.setEncoding('utf8');
+
+    //   res.on('data', function(chunk) {
+    //     console.log('NEW DATA', chunk);
+    //   });
+    // });
+    // req.on('error', function(e) {
+    //   console.log('ERROR ', e.message);
+    // });
+    // console.log('hi');
 
