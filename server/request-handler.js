f0937482459ece1ea2a6f4a14fd872562e22614d
Hack Reactor Student <student@hackreactor.com>
2017-10-30 21:01:52 -0700

    Worked through to POST, working on rendering new posts Commit ROUND 2

--- server/request-handler.js
+++ server/request-handler.js
@@ -15,7 +15,12 @@ var http = require('http');
 var querystring = require('querystring');
 
 
-
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10 // Seconds.
+};
 var data = {
   results: [
     {
@@ -142,12 +147,7 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
+
 module.exports.requestHandler = requestHandler;
 
 
